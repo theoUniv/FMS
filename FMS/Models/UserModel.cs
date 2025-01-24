ee23a05f3e80de78bb246f7c2d7251493d201ac3
theoUniv <theojlemaire@gmail.com>
2025-01-24 22:40:26 +0100

    Login account

---
+++ FMS/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FMS.Models
+{
+    public class UserModel
+    {
+        public int user_id { get; set; }
+
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
--- FMS/Program.cs
+++ FMS/Program.cs
@@ -1,15 +1,24 @@
 using FMS.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Ajouter AppDbContext avec la chaîne de connexion
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Ajouter HttpClient pour les appels HTTP
 builder.Services.AddHttpClient();
 
+// Ajouter le service GitHubService
 builder.Services.AddScoped<GitHubService>();
 
+// Ajouter les contrôleurs avec vues
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Configuration des middlewares
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -22,9 +31,9 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
+// Définir les routes pour les contrôleurs
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
